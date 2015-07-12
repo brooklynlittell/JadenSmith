0403ebe09b3005e06944716d054872a8c1b84fff
BrandonLittell <brandonl3737@gmail.com>
2015-07-12 12:10:25 -0700

    Code style update

--- index.js
+++ index.js
@@ -14,41 +14,41 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
- //Callback functions
-    var error = function (err, response, body) {
-        console.log('ERROR [%s]', err);
+//Callback functions
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
 
-    };
-    var success = function (data) {
-    	var tweets = {};
-    	// make response pretty
-        data = JSON.parse(data);
-        for(tweet in data){
-        	// ignore tweets with a URL in it
-        	if(data[tweet].text.indexOf("http") == -1){
-        		// map text -> count (weight can be used later to pull good tweets)
-        		tweets[data[tweet].text] = data[tweet].retweet_count + data[tweet].favorite_count;
-        	} 
-        }
-        console.log(tweets)
-        // returns the map of the tweets for a given user
-        return tweets;        
-    };
-
-
-    //Get this data from your twitter apps dashboard
-    var config = {
-        "consumerKey": "k1pfjLFmJkT2WKt5IEeOMTpXR",
-        "consumerSecret": "yyEn9IsMQKXnbP8RFWBRAaIz04NaOz6LKnsp5anp6GIPj1PUrA",
+};
+var success = function (data) {
+    var tweets = {};
+    // make response pretty
+    data = JSON.parse(data);
+    for(tweet in data){
+        // ignore tweets with a URL in it
+        if(data[tweet].text.indexOf("http") == -1){
+            // map text -> count (weight can be used later to pull good tweets)
+            tweets[data[tweet].text] = data[tweet].retweet_count + data[tweet].favorite_count;
+        } 
     }
+    console.log(tweets)
+    // returns the map of the tweets for a given user
+    return tweets;        
+};
 
-    var twitter = new Twitter(config);
 
-    //pull in 100 tweets excluding rts and replies
-    var username = 'officialjaden'
-    var tweets = twitter.getUserTimeline({ screen_name: username, count: '100',
-    	exclude_replies: true, include_rts: false}, error, success);
-	
+//Get this data from your twitter apps dashboard
+var config = {
+    "consumerKey": "k1pfjLFmJkT2WKt5IEeOMTpXR",
+    "consumerSecret": "yyEn9IsMQKXnbP8RFWBRAaIz04NaOz6LKnsp5anp6GIPj1PUrA",
+}
+
+var twitter = new Twitter(config);
+
+//pull in 100 tweets excluding rts and replies
+var username = 'officialjaden'
+var tweets = twitter.getUserTimeline({ screen_name: username, count: '100',
+    exclude_replies: true, include_rts: false}, error, success);
+    
    
 
 
