9e2bd96af8e40f742c5dfedc392d6f0b5e5849fb
BrandonLittell <brandonl3737@gmail.com>
2015-07-13 23:40:29 -0700

    Small content updates for views.

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -29,6 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/semantic-ui/dist/semantic.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
