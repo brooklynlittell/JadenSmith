3dabaecb60ae1fbb9468aeed67c167a11fd58cb2
Jen Lamere <jennielamere@gmail.com>
2015-07-15 20:11:09 +0200

    did a bit more

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -7,23 +7,19 @@
  * Controller of the jadenSmithApp
  */
 
-angular.module('jadenSmithApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
 
 angular.module('jadenSmithApp')
 .controller('MainCtrl', function($scope, $resource) {
   $scope.user = 'officialjaden';
+  $scope.isSearch = false;
   $scope.searchTweets = function(){
     $scope.tweets = $resource("http://localhost:5000/tweets").get();
+    $scope.isSearch = true;
+    console.log($scope.isSearch);
   };
 
   $scope.$on('$viewContentLoaded', function () {
     $scope.image = $resource("http://localhost:5000/images").get();
   });
 });
+
