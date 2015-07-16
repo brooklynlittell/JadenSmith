56691593273726444ff7b4cd296648ebf81a98da
BrandonLittell <brandonl3737@gmail.com>
2015-07-15 21:34:26 -0700

    Built up UI control

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -11,15 +11,44 @@
 angular.module('jadenSmithApp')
 .controller('MainCtrl', function($scope, $resource) {
   $scope.user = 'officialjaden';
+  $scope.tweetList = null;
+  $scope.imageList = null;
+  $scope.currentTweet = 0;
+  $scope.currentImage = 0;
   $scope.isSearch = false;
-  $scope.searchTweets = function(){
-    $scope.tweets = $resource("http://localhost:5000/tweets").get();
+
+  $scope.getTweets = function(){
+    $scope.tweetList = $resource("http://localhost:5000/tweets").get();
     $scope.isSearch = true;
     console.log($scope.isSearch);
   };
 
+  $scope.newImage = function(){
+  	$scope.currentImage++;
+  	// Loop images
+  	if($scope.currnetImage >= Object.keys($scope.imageList).length)
+  	{
+  		$scope.currentImage = 0;
+  	}
+  };
+
+  $scope.generateImage = function(){
+  	
+  };
+
+  $scope.onSearchUser = function(){
+  	$scope.getTweets(/*user*/);
+  	$scope.generateImage();
+  };
+
+  $scope.onNewImage = function(){
+  	$scope.newImage();
+  	$scope.generateImage();
+  };
+
   $scope.$on('$viewContentLoaded', function () {
-    $scope.image = $resource("http://localhost:5000/images").get();
+    $scope.imageList = $resource("http://localhost:5000/images").get();
+    $scope.getTweets();
   });
 });
 
