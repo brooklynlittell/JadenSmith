02e0af4c65ced216c4b6af1f886839b84d9a50b6
Jen Lamere <jennielamere@gmail.com>
2015-07-18 11:20:31 +0200

    fixed image async bug

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/semantic-ui/dist/semantic.js',
+      'bower_components/color-thief/src/color-thief.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
